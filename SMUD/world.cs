e9a0e3f2bc8d9628407672ea85018fde41832b51
Emil <Emil@LAPTOP-2H7F9CHA>
2016-09-28 10:05:27 +0200

    Add project files.

---
+++ SMUD/world.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMUD
+{
+    public class world
+    {
+        List<Room> rooms;
+        public world()
+        {
+            
+        }
+    }
+}
